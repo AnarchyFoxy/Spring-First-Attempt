5a76bc51145a9f20e8882d148639c7cb4ec3c7ac
Astryda Malinowska <astamalinowska@outlook.com>
2023-07-03 16:45:08 +0200

    initial commit

---
+++ src/test/java/com/ZeroDivisionError/FirstAttempt/FirstAttemptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZeroDivisionError.FirstAttempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAttemptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
