49252ef0346f52e0dd2b3db7530606c4d11851cb
Astryda Malinowska <astamalinowska@outlook.com>
2023-07-03 16:51:20 +0200

    Added Hello World page

--- src/main/java/com/ZeroDivisionError/FirstAttempt/FirstAttemptApplication.java
+++ src/main/java/com/ZeroDivisionError/FirstAttempt/FirstAttemptApplication.java
@@ -2,12 +2,21 @@ package com.ZeroDivisionError.FirstAttempt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstAttemptApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstAttemptApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "Anarchy Foxy World!") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
